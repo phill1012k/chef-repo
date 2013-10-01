4a1cfd6f242f74893224489f7902c53d12b94cef
dpoultney <david.poultney@aeriandi.com>
2013-10-01 17:36:52 +0100

    added ntp.conf.erb

--- cookbooks/ntp/recipes/default.rb
+++ cookbooks/ntp/recipes/default.rb
@@ -6,3 +6,11 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+package 'ntp'
+template '/etc/ntp.conf' do
+  source    'ntp.conf.erb'
+  notifies  :restart, 'service[ntp]'
+end
+service 'ntp' do
+  action [:enable, :start]
+end
